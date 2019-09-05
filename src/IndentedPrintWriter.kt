2aaa9b6b6bd05d539afb50382393a3724977a688
Colin Holzman <colin.holzman@protonmail.com>
2019-09-05 07:47:35 -0400

    add not implemented for some methods of IndentedPrintWriter

--- src/IndentedPrintWriter.kt
+++ src/IndentedPrintWriter.kt
@@ -5,13 +5,29 @@ import java.nio.charset.Charset
 
 class IndentedPrintWriter(writer: Writer): PrintWriter(writer) {
 
-    var indent = 0
+    private var indent = 0
         set(value) {
             field = value
             currentIndent = singleIndent.repeat(value)
         }
     private val singleIndent = "    "
     private var currentIndent = ""
+
+    override fun write(c: Int) {
+        throw NotImplementedError()
+    }
+
+    override fun write(buf: CharArray, off: Int, len: Int) {
+        throw NotImplementedError()
+    }
+
+    override fun write(buf: CharArray) {
+        throw NotImplementedError()
+    }
+
+    override fun write(s: String, off: Int, len: Int) {
+        super.write(s, off, len)
+    }
 
     override fun write(s: String) {
         val indented = s.prependIndent(currentIndent)
